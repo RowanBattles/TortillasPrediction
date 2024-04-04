f94afe6b6c118e8d29f5dfd6b3dc291add6d54b1
Rowan <rowanvanderweel@gmail.com>
2024-04-04 20:57:26 +0200

    working

---
+++ Dis.js
@@ -0,0 +1,129 @@
+{
+    "payload": {
+        "type": "FeatureCollection",
+        "features": [
+            {
+                "id": "7004789",
+                "type": "Feature",
+                "geometry": {
+                    "type": "MultiLineString",
+                },
+                "properties": {
+                    "stations": [
+                        "TBU",
+                        "TB",
+                        "OT",
+                        "BTL",
+                        "BET",
+                        "EHS",
+                        "EHV"
+                    ],
+                    "niveau": "MINDER_TREINEN",
+                    "disruptionType": "WERKZAAMHEID"
+                }
+            },
+            {
+                "id": "7004790",
+                "type": "Feature",
+                "geometry": {
+                    "type": "MultiLineString",
+                },
+                "properties": {
+                    "stations": [
+                        "GVC",
+                        "GV",
+                        "GVMW",
+                        "RSW",
+                        "DT",
+                        "DTCP",
+                        "SDM",
+                        "RTD",
+                        "RTB",
+                        "RTZ",
+                        "RLB",
+                        "BDPB",
+                        "BD",
+                        "GZ",
+                        "TBR",
+                        "TBU",
+                        "TB",
+                        "OT",
+                        "BTL",
+                        "BET",
+                        "EHS",
+                        "EHV"
+                    ],
+                    "niveau": "MINDER_TREINEN",
+                    "disruptionType": "WERKZAAMHEID"
+                }
+            },
+            {
+                "id": "7004805",
+                "type": "Feature",
+                "geometry": {
+                    "type": "MultiLineString",
+                },
+                "properties": {
+                    "stations": [
+                        "UT",
+                        "UTVR",
+                        "UTLN",
+                        "HTN",
+                        "HTNC",
+                        "CL",
+                        "GDM",
+                        "ZBM",
+                        "HT",
+                        "VG",
+                        "BTL",
+                        "BET",
+                        "EHS",
+                        "EHV"
+                    ],
+                    "niveau": "MINDER_TREINEN",
+                    "disruptionType": "WERKZAAMHEID"
+                }
+            },
+            {
+                "id": "7004812",
+                "type": "Feature",
+                "geometry": {
+                    "type": "MultiLineString",
+                },
+                "properties": {
+                    "stations": [
+                        "EHV",
+                        "HMBV",
+                        "HMH",
+                        "HM"
+                    ],
+                    "niveau": "MINDER_TREINEN",
+                    "disruptionType": "WERKZAAMHEID"
+                }
+            },
+            {
+                "id": "7004796",
+                "type": "Feature",
+                "geometry": {
+                    "type": "MultiLineString",
+                },
+                "properties": {
+                    "stations": [
+                        "BD",
+                        "GZ",
+                        "TBR",
+                        "TBU",
+                        "TB",
+                        "OT",
+                        "BTL",
+                        "BET",
+                        "EHS",
+                        "EHV"
+                    ],
+                    "niveau": "MINDER_TREINEN",
+                    "disruptionType": "WERKZAAMHEID"
+                }
+            }
+        ]
+    }
+}
